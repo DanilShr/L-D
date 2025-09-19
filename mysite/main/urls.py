49e58f26f7d2c6d120184500dc319f184b30bd0f
Shred <daerrey3@gmail.com>
2025-09-20 06:59:07 +1100

    add new style

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
 
     path("servises", ServicesView.as_view(), name="services"),
 
-    path("products/", ProductView.as_view(), name="products"),
+    path("products", ProductView.as_view(), name="products"),
 
     path("products/<int:pk>", ProductDetailView.as_view(), name="product"),
 
