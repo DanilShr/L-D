026040d8c52f5d61886748a02236542edaf45a47
Shred <daerrey3@gmail.com>
2025-09-19 23:40:57 +1100

    rebase projects

--- mysite/src/scripts/script.js
+++ mysite/src/scripts/script.js
@@ -13,3 +13,21 @@ window.onclick = function(event) {
         closeModal();
     }
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Вешаем обработчик на document, который сработает при изменении любого file input
+    document.addEventListener('change', function(event) {
+        if (event.target.id === 'file-input' && event.target.files[0]) {
+            const file = event.target.files[0];
+            const avatarPreview = document.getElementById('avatar-preview');
+
+            if (avatarPreview && file.type.startsWith('image/')) {
+                const reader = new FileReader();
+                reader.onload = function(e) {
+                    avatarPreview.src = e.target.result;
+                };
+                reader.readAsDataURL(file);
+            }
+        }
+    });
+});
